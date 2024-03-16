12eeb8e3aa4321d50f28ced599c7bedd02c55f6c
antonioamaral <antonio.amaral@sensedia.com>
2024-03-16 10:07:20 -0300

    refactor

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,12 +1,15 @@
+def reverse_list(head):
+    anterior = None
+    atual = head
+
+    while atual: # != None
+        next = atual.next
+        atual.next = anterior
+
+        anterior = atual
+        atual = next
+    return anterior
+
+
 class Solution(object):
-    def reverseList(self, head):
-        anterior = None
-        atual = head
-
-        while atual: # != None
-            next = atual.next
-            atual.next = anterior
-
-            anterior = atual
-            atual = next
-        return anterior
+    pass
