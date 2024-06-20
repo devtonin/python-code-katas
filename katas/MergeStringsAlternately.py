038c1c7e393075f4702b3b38a626ce0e0eaa3d6e
devtonin <antonio.amaral2406@gmail.com>
2024-06-20 10:16:16 -0300

    added katas

---
+++ katas/MergeStringsAlternately.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        size = 0;
+
+        if (word1.__sizeof__ == word2.__sizeof__):
+            size = word1.__sizeof__
+        else:
+            (max(word1.__sizeof__, word2.__sizeof__)) 
+
+
+        for i in size:
+            if (i < word1.__sizeof__):
+                        
